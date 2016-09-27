0d7be71c318115a0c1a07de497890d8a6822028b
INSPIRE-REX <inspirerex@gmail.com>
2016-09-27 07:44:29 +0530

    correct path

--- cardinal.mk
+++ cardinal.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/motorola/condor/cardinal_condor.mk)
 
 # Inherit some common Cardinal-AOSP stuff.
-$(call inherit-product, vendor/cardinal/config/common.mk)
+$(call inherit-product, vendor/cardinal/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cardinal_condor
